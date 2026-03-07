66004f2f193d3a2a40329448c1c0cc541f4766f2
Muneeb Rehman <muneebbhanbhro9@gmail.com>
2026-03-07 12:47:29 +0500

    Refactor Transactions and Budget classes

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 
-class Category; // forward decalaration, because 'Category' is used before it is defined in 'Transactions'
+class Category; // forward decalaration, because 'Category' is used before it is defined in 'Transactions', line 14
 
 class Transactions
 {
@@ -14,9 +14,9 @@ class Transactions
 		Category* category;
 		
 	public:
-		Transactions(double amount, std::string name, std::string details, Category *category/* , date and time*/) : amount(amount), name(name), details(details), category(category) {}
+		Transactions(std::string name, double amount, std::string details/*, Category *category/* , date and time*/) : amount(amount), name(name), details(details)/*, category(category)*/ {}
 		
-		double getAmount()  // in order to update the budget
+		double getAmount()  // in order to updating/editing
 		{
 			return amount;
 		}
@@ -28,7 +28,30 @@ class Category
 		std::string name;
 	
 	public:
-		Category(std::string name) : name(name){
+		Category(std::string name) : name(name){}
+		
+		std::string getName()
+		{
+			return name;
+		}
+		
+		void addTransaction()
+		{
+			std::string name, details, catName;
+		//	Category* category;
+			double amount;
+			std::cout << "\nEnter the name, amount, details, category";
+			std::cin >> name >> amount >> details/* >> category*/; 
+			
+			std::cout << "\nEnter amount: ";
+			std::cin >> amount;
+			
+			std::cout << "\nEnter details: ";
+			std::cin >> details;
+			
+			std::cout << "\nEnter category: ";
+			std::cin >> catName;
+//			Transactions* t(name, amount, details, *category);
 		}
 		
 };
@@ -40,14 +63,34 @@ class Budget // for multiple budgets
 		double limit;
 //		double daily;  // a daily spending limit calculated using limit and budget duration, exceeding this will cause it  to be recalculated
 //		int duration  // to-do
-		std::vector <Transactions*> transactionVctr;  // a vector to store the list of pointers to transactions
+		std::vector <Transactions*> transactionVctr;  // a vector to store pointers to 'Transactions' objects
+		std::vector <Category*> categoryVctr; // a vector to store pointers to 'Category' objects
 		
 	public:
 		Budget(std::string name, double limit) : name(name), limit(limit) {}
 		
-		void addTransaction(Transactions* t)
+		Category* findCategory(std::string catName)  // to search if a category already exists
 		{
-			transactionVctr.push_back(t);
+			for (Category* c : categoryVctr)
+			{
+				if (catName == c->getName())
+				{
+					return c;
+				}
+			}
+			
+			return nullptr; // not found, i.e; create new category
+		}
+		
+		void addCategory(std::string catName)
+		{
+			Category* catPtr = findCategory(catName);
+			
+			if (catPtr == nullptr)
+				categoryVctr.push_back(new Category(catName));
+				
+			else
+				std::cout << "\nCategory already exists!";
 		}
 };
 
@@ -63,21 +106,35 @@ If your code has access to the Singleton class, then it’s able to call the Sin
 class BudgetManager
 {
 	private:
-		BudgetManager();
-		static BudgetManager& getInstance()
-		{
-			BudgetManager instance;
-			return instance;
-		}
-		std::vector <Budget*> budgetVctr;
+		BudgetManager() { std::cout << "Called\n";};
+		std::vector <Budget*> budgetVctr;  // vector to store pointers to 'Budget' objects
+		
+		BudgetManager(const BudgetManager&) = delete;  // deleting copy constructor
+		BudgetManager& operator=(const BudgetManager&) = delete; // deleting assignment operator
 		
 	public:
+		static BudgetManager& getInstance()  // a static mathod to call for
+		{
+			static BudgetManager instance;  // static ensures that its created only once along with the private constructor
+			return instance;
+		}
 };
 
 int main()
 {
 	std::cout << "\n-------- FinaLT --------\n";
 	
-	BudgetManager manager;
+	BudgetManager& manager = BudgetManager::getInstance();  // used a reference because it points to the same instance. Without it, the copy constructor will be invoked, which has been deleted for this class
+	
+	// main menu
+	int choice;
+	std::cout << "1. Add Transaction \t 2. Create a new budget\n";
+	std::cin >> choice;
+	
+//	switch (choice)
+//	{
+//		case 1:
+//			
+//	}
 	
 }
